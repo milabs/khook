e6d2dac281c39c765b6207f3013a20031c0945da
Ilya V. Matveychikov <matvejchikov@gmail.com>
2024-02-26 00:58:14 +0300

    khook_demo: fix node_permission hook example

--- khook_demo/main.c
+++ khook_demo/main.c
@@ -1,5 +1,6 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/version.h>
 
 #include <khook/engine.h>
 
@@ -10,6 +11,33 @@
 #include <linux/fs.h>
 
 KHOOK(inode_permission);
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6,3,0)
+
+static int khook_inode_permission(struct mnt_idmap *idmap, struct inode *inode, int mask)
+{
+	int ret = 0;
+
+	ret = KHOOK_ORIGIN(inode_permission, idmap, inode, mask);
+	printk("%s(%p, %08x) = %d\n", __func__, inode, mask, ret);
+
+	return ret;
+}
+
+#elif LINUX_VERSION_CODE >= KERNEL_VERSION(5,12,0)
+
+static int khook_inode_permission(struct user_namespace *mnt_userns, struct inode *inode, int mask)
+{
+	int ret = 0;
+
+	ret = KHOOK_ORIGIN(inode_permission, mnt_userns, inode, mask);
+	printk("%s(%p, %08x) = %d\n", __func__, inode, mask, ret);
+
+	return ret;
+}
+
+#else
+
 static int khook_inode_permission(struct inode *inode, int mask)
 {
 	int ret = 0;
@@ -19,6 +47,8 @@ static int khook_inode_permission(struct inode *inode, int mask)
 
 	return ret;
 }
+
+#endif
 
 ////////////////////////////////////////////////////////////////////////////////
 // An example of using KHOOK_EXT
